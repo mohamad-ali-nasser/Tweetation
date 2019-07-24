86d5493a16838d0e5681bc5456acedf7d3b7f3b7
cartman12 <mhd.ali.nasser@gmail.com>
2019-07-24 22:22:48 +0300

    models.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -3,11 +3,17 @@
 Main application and routing logic for Tweetation
 
 """
+
+
 from flask import Flask
+from .models import DB
+
 
 def create_app():
 	"""Create and configure an instance of the Flask application."""
 	app = Flask(__name__)
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+	DB.init_app(app)
 
 	@app.route('/')
 	def root():
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,5 +1,5 @@
 """ SQLAlchemy models for twitoff."""
-from flask_sqlalchemy import SQLalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
@@ -8,7 +8,15 @@ class User(DB.Model):
 	id = DB.Column(DB.Integer, primary_key=True)
 	name = DB.Column(DB.String(15), nullable=False)
 
+	def __repr__(self):
+		return '<User {}>'.format(self.name)
+
 class Tweet(DB.Model):
 	"""Tweets"""
-	id = DB.column(DB.Integer, primary_key=True)
+	id = DB.Column(DB.Integer, primary_key=True)
 	text = DB.Column(DB.Unicode(280))
+	user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+	user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+	def __repr(self):
+		return '<Tweet {}>'.format(self.text)
