61d8221ce8e191f151caaf4829445d5fb3d3d4cc
cartman12 <mhd.ali.nasser@gmail.com>
2019-07-25 15:46:54 +0300

    Twitter API hooked, env added to gitignore

--- twitoff/app.py
+++ twitoff/app.py
@@ -4,20 +4,28 @@ Main application and routing logic for Tweetation
 
 """
 
-
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 
 def create_app():
 	"""Create and configure an instance of the Flask application."""
 	app = Flask(__name__)
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+	app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+	app.config['ENV'] = config('ENV')
 	DB.init_app(app)
 
 	@app.route('/')
 	def root():
-		return 'Welcome to Tweetation!'
+		users = User.query.all()
+		return render_template('base.html', title='Home', users=users)
+	
+	@app.route('/reset')
+	def reset():
+		DB.drop_all()
+		DB.create_all()
+		return render_template('base.html', title='DB Reset', users=[])
 
 
 	return app
